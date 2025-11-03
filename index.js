963f7034798cf2241a02d6097f8951d7d3d72415
Artniaina <kanto.andriahariniaina@gmail.com>
2025-11-03 09:35:50 +0300

    Fix import statement for particulier and add intervenant

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import cors from "cors";
-import prestatairesRoutes from "./routes/particulier.js";
+import particulierRoutes from "./routes/particulier.js";
 import metaRoutes from "./routes/meta.js";
-
+import intervenantRoutes from "./routes/intervenant.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/particuliers", prestatairesRoutes);
+app.use("/particuliers", particulierRoutes);
 app.use("/meta", metaRoutes);
-
+app.use("/intervenants", intervenantRoutes);
 app.listen(2083, () => {
   console.log("Connected to backend!" + " on port 2083");
 });
